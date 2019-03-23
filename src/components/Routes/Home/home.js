a60ee3f60752fbdbdd8236a16a30a8578f885128
Amritans Pandey <amritansh.pandey@tryduit.com>
2019-03-24 02:07:15 +0530

    Routes Initial

---
+++ src/components/Routes/Home/home.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./custom.css";
+import TopBar from "./TopBar/TopBar";
+import BottomBar from "./BottomBar/BottomBar";
+import Karousel from "./Carousel/Carousel";
+import GameCategory from "./GamesCategory/GameCategory";
+import PopularEvents from "./PopularEvents/PopularEvents";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <TopBar />
+        <Karousel />
+        <GameCategory />
+        <PopularEvents />
+        <BottomBar />
+      </div>
+    );
+  }
+}
+
+export default Home;
